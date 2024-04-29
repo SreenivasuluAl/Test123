5fe54dfc579c148954eb2858987d8daeee4f147b
sreenualakuntla3@gmail.com <sreenualakuntla3@gmail.com>
2024-04-30 00:59:57 +0530

    added new function

---
+++ file_1.c
@@ -0,0 +1,26 @@
+#include <studio.h>
+int main() {
+
+int number1,number2, sum;
+
+printf("enter two integers: "):
+scanf ("%d%d", &number1, &number2);
+
+sum = number1+ number3;
+
+printf("%d + %d =%d", number1, number2, sum);
+return 0;
+}
+
+
+int addfunc(int a,int b)
+{
+int c =a+b;
+return c;
+}
+
+int func_add(int a, int b)
+{
+return a+b;
+}
+
