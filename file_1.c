f6c8b3c9a5ee226416c80a184fb7370a79795bf9
sreenualakuntla3@gmail.com <sreenualakuntla3@gmail.com>
2024-04-30 01:05:17 +0530

    adding same function from main branch

---
+++ file_1.c
@@ -0,0 +1,18 @@
+#include <studio.h>
+int main() {
+
+int number1,number2, sum;
+
+printf("enter two integers: "):
+scanf ("%d%d", &number1, &number2);
+
+sum = number1+ number3;
+
+printf("%d + %d =%d", number1, number2, sum);
+return 0;
+}
+
+int func_add(int a, int b)
+{
+return a+b;
+}
